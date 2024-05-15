e1d45153dc1aa91153e59ddf815bccb965e6b248
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-15 21:57:33 +0800

    "adasdasd"

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -2,7 +2,8 @@
   <div class="p-4 m-4 text-sm rounded-lg shadow-lg" :class="computedClasses"  role="alert">
     <svg v-if="icon" class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-    </svg>{{ msg }}
+    </svg>
+    {{ msg }}
   </div>
 </template>
 
