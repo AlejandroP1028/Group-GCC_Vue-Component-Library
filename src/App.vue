0fbff8d1bc3db8a4f2aa82d37897f8e08bddc7e4
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-15 15:30:17 +0800

    added alert sample component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
+    <alert_component msg="WAWAWAWA"></alert_component>
 
 </template>
 
 <script>
+import alert_component from './components/Alert.vue';
 
-
+export default {
+  name: 'App',
+  components: {
+    alert_component
+  }
+}
 </script>
 
---
+++ src/components/Alert.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> {{ msg }}
+    </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'alert_component',
+  props: {
+    msg: String
+  }
+}
+</script>
