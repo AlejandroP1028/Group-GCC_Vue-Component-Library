261217bdfd77f024fd83e7faa8bbffb2293dc787
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-17 03:19:08 +0800

    changed styles to sky,cyan, and teal -- added 'darkmode' styles -- altered app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class=" overflow-hidden bg-gray-100 h-screen w-screen">
-    <alert_component msg="ooga booga 123" textColor="white" type="secondary" position="tc"></alert_component>
-    <alert_component msg="ooga booga 123" textColor="white" type="secondary" position="lc"></alert_component>
+  <div :class="{ 'dark': isDarkMode }" class="overflow-hidden h-screen w-screen bg-gray-100 dark:bg-gray-900">
+    <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+      {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+    </button>
+    <alert_component msg="ooga booga 123" type="default" position="tl" :isDark="isDarkMode" :bordered="true"></alert_component>
+    <alert_component msg="ooga booga 123" type="sky" position="tr" :isDark="isDarkMode" :bordered="true"></alert_component>
+    <alert_component msg="ooga booga 123" type="cyan" position="ll" :isDark="isDarkMode" :bordered="true"></alert_component>
+    <alert_component msg="ooga booga 123" type="teal" position="lr" :isDark="isDarkMode" :bordered="true"></alert_component>
   </div>
-
 </template>
 
 <script>
@@ -13,7 +17,25 @@ export default {
   name: 'App',
   components: {
     alert_component
+  },
+  data() {
+    return {
+      isDarkMode: false // Initial state is light mode
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      // Toggle dark mode
+      this.isDarkMode = !this.isDarkMode;
+    }
   }
 }
 </script>
 
+<style scoped>
+/* Dark mode styles */
+.dark {
+  background-color: #1a202c;
+  color: #ffffff;
+}
+</style>
--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -6,7 +6,7 @@
   viewBox="0 0 24 24"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
->
+  >
   <path
     d="M11 10.9794C11 10.4271 11.4477 9.97937 12 9.97937C12.5523 9.97937 13 10.4271 13 10.9794V16.9794C13 17.5317 12.5523 17.9794 12 17.9794C11.4477 17.9794 11 17.5317 11 16.9794V10.9794Z"
     fill="currentColor"
@@ -21,7 +21,7 @@
     d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12Z"
     fill="currentColor"
   />
-</svg>
+  </svg>
     {{ msg }}
   </div>
 </template>
@@ -33,17 +33,20 @@ export default {
     msg: String,
     type: {
       type: String,
-      validator: function (value) {
-        return ['default', 'secondary'].includes(value);
+      validator: function(value) {
+        return ['default', 'sky', 'cyan', 'teal'].includes(value);
       },
       default: 'default'
     },
     size: {
       type: String,
-      validator: function (value) {
-        return ['w', 's','m','l'].includes(value);
+      validator: function(value) {
+        return ['w', 's', 'm', 'l'].includes(value);
       },
       default: 'w',
+    },
+    isDark: {
+      type: Boolean,
     },
     icon: {
       type: Boolean,
@@ -53,16 +56,9 @@ export default {
       type: Boolean,
       default: false,
     },
-    textColor: {
-      type: String,
-      validator: function (value) {
-        return ["red","white","yellow"].includes(value)
-      },
-      default: 'white',
-    },
     position: {
       type: String,
-      validator: function (value) {
+      validator: function(value) {
         return ['tl', 'tc', 'tr', 'll', 'lc', 'lr'].includes(value);
       },
       default: 'tl',
@@ -76,40 +72,59 @@ export default {
                      this.position === 'll' ? 'bottom-0 left-0' :
                      this.position === 'lc' ? 'bottom-0 left-1/2  transform -translate-x-1/2' :
                      this.position === 'lr' ? 'bottom-0 right-0' :
-                     ''
+                     '';
+
+      const styleClasses = {
+        default: {
+          bgClass: this.isDark ? 'bg-gray-800' : 'bg-blue-50',
+          textClass: this.isDark ? 'text-blue-400' : 'text-blue-800',
+          borderClass: this.bordered ? 'border ' + (this.isDark ? 'border-blue-400' : 'border-blue-800') : '',
+        },
+        sky: {
+          bgClass: this.isDark ? 'bg-gray-800' : 'bg-sky-50',
+          textClass: this.isDark ? 'text-sky-400' : 'text-sky-800',
+          borderClass: this.bordered ? 'border ' + (this.isDark ? 'border-sky-400' : 'border-sky-800') : '',
+        },
+        cyan: {
+          bgClass: this.isDark ? 'bg-gray-800' : 'bg-cyan-50',
+          textClass: this.isDark ? 'text-cyan-400' : 'text-cyan-800',
+          borderClass: this.bordered ? 'border ' + (this.isDark ? 'border-cyan-400' : 'border-cyan-800') : '',
+        },
+        teal: {
+          bgClass: this.isDark ? 'bg-gray-800' : 'bg-teal-50',
+          textClass: this.isDark ? 'text-teal-400' : 'text-teal-800',
+          borderClass: this.bordered ? 'border ' + (this.isDark ? 'border-teal-400' : 'border-teal-800') : '',
+        }
+      };
+
+      const { bgClass, textClass, borderClass } = styleClasses[this.type] || styleClasses['default'];
+
       return [
-        this.type === 'secondary' ? 'bg-blue-400' : 'bg-blue-600',
-        this.textColor === 'white' ? 'text-zinc-200' : this.textColor === 'red' ? 'text-red-400' : 'text-yellow-400',
-        this.textColor === 'white' ? 'text-zinc-200' : this.textColor === 'red' ? 'text-red-400' : 'text-yellow-400',
-        this.bordered ? this.textColor === 'white' ? 'border border-zinc-200' : this.textColor === 'red' ? 'border border-red-400' : 'border border-yellow-400' : '',
+        bgClass, textClass, borderClass,
         this.size === 'w' ? 'inline-block' : this.size === 's' ? 'w-32' : this.size === 'm' ? 'w-48' : 'w-64',
         position,
-        this.show ? 'end' : this.position + '-start' // Add starting class based on the position if show is true
+        this.show ? 'end' : this.position + '-start'
       ];
     }
   },
   data() {
     return {
       out: true,
-      show: false // Initially set show to false
+      show: false
     }
   },
   mounted() {
-  // Use setTimeout to delay showing the component
-  setTimeout(() => {
-    this.show = true; // Set show to true after 0.05 seconds
-  }, 50);
-  
-  setTimeout(() => {
-    // Set show to false after 5 seconds
-    this.show = false;
-    // Wait for the transition to end before removing the component
     setTimeout(() => {
-      this.out = false; // Remove the component from the DOM
-    }, 300); // 300 milliseconds is the duration of the transition
-  }, 5000);
-},
+      this.show = true;
+    }, 50);
 
+    setTimeout(() => {
+      this.show = false;
+      setTimeout(() => {
+        this.out = false;
+      }, 300);
+    }, 5000);
+  },
 }
 </script>
 
