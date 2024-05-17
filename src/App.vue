7159b43849430f8712d0e18970bdf7f91038684b
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-17 22:27:28 +0800

    modified:   src/App.vue 	new file:   src/components/ItemList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-
-</template>
-
-<script>
-
-
-</script>
-
+    <div class="overflow-hidden h-screen w-screen bg-gray-100">
+      <ItemList />
+    </div>
+  </template>
+  
+  <script>
+  import ItemList from './components/ItemList.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      ItemList
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add any global styles here */
+  </style>
+  
---
+++ src/components/ItemList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl mb-4">List Group</h1>
+    <h1 class="text-2xl mb-4">List of Items</h1>
+    <ul class="list-disc pl-5 space-y-2">
+      <li v-for="item in items" :key="item" class="bg-white p-2 rounded shadow">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemList',
+  data() {
+    return {
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+    };
+  }
+}
+</script>
+
+<style scoped>
+li {
+  transition: background-color 0.3s;
+}
+
+li:hover {
+  background-color: #ebf8ff; 
+}
+
+li:focus {
+  box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5); 
+}
+</style>
