79b736dd881fe5d0e590347c6c5344cb426bf15d
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-20 03:27:47 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from "./App.vue";
-import './index.css'
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Carousel from 'primevue/carousel';
+import Button from 'primevue/button';
+import Tag from 'primevue/tag';
+import 'primevue/resources/themes/saga-blue/theme.css'; // Choose a theme
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component('Carousel', Carousel);
+app.component('Button', Button);
+app.component('Tag', Tag);
+app.use(BootstrapVue3)
+
+app.mount('#app');
