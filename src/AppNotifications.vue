afca5fd1208e239c75e64cec83853c8f15e31109
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-20 23:02:52 +0800

    modified:   src/AppNotifications.vue modified:   src/components/NotificationsGroup.vue

--- src/AppNotifications.vue
+++ src/AppNotifications.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-center h-screen w-screen">
+    <div class="flex">
       <Notifications />
     </div>
 </template>
--- src/components/NotificationsGroup.vue
+++ src/components/NotificationsGroup.vue
@@ -1,26 +1,44 @@
 <template>
-  <div class="h-full w-screen bg-blue-100">
-    <div class="flex flex-col items-center justify-start h-full w-screen mt-7">
+  <div :class="[pageBackground, textColor]">
+    <div class="flex flex-col items-center justify-start h-screen w-screen mt-7">
       <div class="text-justify mb-4 max-w-4xl">
-        <h1 class="text-4xl mb-2 font-bold">Notifications</h1>
-        <p class="mb-4">
+        <h1 :class="['text-4xl', 'mb-2', 'font-bold', headingColor]">Notifications</h1>
+        <p :class="['mb-4', paragraphColor]">
           The notification component supports different types of notifications such as success, error, warning, and info messages, making it versatile and essential for user communication. Here are the various designs or formats a notification can take:
         </p>
-        <h1 class="text-2xl mb-2 font-bold">Default Notifications</h1>
+        <h1 :class="['text-2xl', 'mb-2', 'mt-4', 'font-bold', 'text-center', headingColor]">Default Notifications</h1>
       </div>
-      <div class="max-w-3xl w-full p-4 bg-white rounded-3xl shadow-lg">
-        <div class="flex justify-center">
-          <button class="border border-blue-200 text-blue-900 px-4 py-2 mr-2 rounded hover:bg-blue-300 mb-2" @click="showNotification('Notification Light', 'This is a notification in light mode.')">Notification Light</button>
-          <button class="bg-blue-900 text-white px-4 py-2 mr-2 rounded hover:bg-blue-400 mb-2" @click="showNotification('Notification Dark', 'This is a notification in dark mode.', 'dark')">Notification Dark</button>
-          <button class="bg-blue-800 text-white px-4 py-2 mr-2 rounded hover:bg-blue-500 mb-2" @click="showNotification('Primary Notification', 'This is a primary notification.', 'success')">Notification Primary</button>
-          <button class="bg-blue-700 text-white px-4 py-2 mr-2 rounded hover:bg-blue-600 mb-2" @click="showNotification('Success Notification', 'This is a success notification.', 'success')">Notification Success</button>
-          <button class="bg-blue-600 text-white px-4 py-2 mr-2 rounded hover:bg-blue-700 mb-2" @click="showNotification('Danger Notification', 'This is a danger notification.', 'danger')">Notification Danger</button>
-          <button class="bg-blue-500 text-white px-4 py-2 mr-2 rounded hover:bg-blue-800 mb-2" @click="showNotification('Warning Notification', 'This is a warning notification.', 'warning')">Notification Warning</button>
+      <div :class="['w-full', 'max-w-xl', 'p-4', 'rounded-3xl', 'shadow-xl', containerBackground]">
+        <div class="flex justify-center flex-wrap">
+          <button class="border mt-4 mb-4 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showNotification('Notification Light', 'This is a notification in light mode.', 'light')">Notification Light</button>
+          <button class="bg-gray-800 mt-4 mb-4 text-sky-400 px-4 py-2 mr-2 rounded hover:bg-blue-900" @click="showNotification('Notification Dark', 'This is a notification in dark mode.', 'dark')">Notification Dark</button>
         </div>
       </div>
       <div class="notification-container">
-        <div v-for="notification in notifications" :key="notification.id" :class="['notification', notification.type]" v-show="notification.visible" @transitionend="removeNotification(notification.id)">
+        <div v-for="notification in notifications" :key="notification.id" :class="['notification', notification.type]" v-show="notification.visible" :style="notification.style" @transitionend="removeNotification(notification.id)">
           <strong>{{ notification.title }}</strong>: {{ notification.text }}
+        </div>
+      </div>
+      <div class="text-justify mb-4 max-w-4xl">
+        <h1 :class="['text-2xl', 'mb-2', 'mt-4', 'font-bold', headingColor]">Notification Positions</h1>
+      </div>
+      <div :class="['w-full', 'max-w-xl', 'p-4', 'rounded-3xl', 'shadow-xl', containerBackground]">
+        <div class="flex justify-center flex-wrap">
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a top right notification.', 'light', 'top-right')">Top Right</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a top left notification.', 'light', 'top-left')">Top Left</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a bottom right notification.', 'light', 'bottom-right')">Bottom Right</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a bottom left notification.', 'light', 'bottom-left')">Bottom Left</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a center notification.', 'light', 'center')">Center</button>
+        </div>
+      </div>
+      <div class="text-justify mb-4 max-w-4xl">
+        <h1 :class="['text-2xl', 'mb-2', 'mt-4', 'font-bold', headingColor]">Notification Icons</h1>
+      </div>
+      <div :class="['w-full', 'max-w-xl', 'p-4', 'rounded-3xl', 'shadow-xl', containerBackground]">
+        <div class="flex justify-center flex-wrap">
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a top right notification.', 'light', 'top-right')">Notification Icon Mail</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a top left notification.', 'light', 'top-left')">Notification Icon Error</button>
+          <button class="border mt-2 mb-2 border-blue-800 text-blue-800 px-4 py-2 mr-2 rounded hover:bg-blue-300" @click="showPositionNotification('Notification', 'This is a bottom right notification.', 'light', 'bottom-right')">Notification Icon Chat</button>
         </div>
       </div>
     </div>
@@ -33,7 +51,11 @@ export default {
     return {
       notifications: [],
       notificationId: 0,
-      timeoutId: null
+      timeoutId: null,
+      pageBackground: 'bg-blue-200',
+      headingColor: 'text-blue-800',
+      paragraphColor: 'text-blue-800',
+      containerBackground: 'bg-blue-100'
     };
   },
   methods: {
@@ -47,6 +69,48 @@ export default {
       };
 
       this.notifications.unshift(notification);
+
+      if (type === 'dark') {
+        this.pageBackground = 'bg-gray-800';
+        this.headingColor = 'text-sky-400';
+        this.paragraphColor = 'text-sky-400';
+        this.containerBackground = 'bg-gray-700';
+      } else if (type === 'light') {
+        this.pageBackground = 'bg-blue-200';
+        this.headingColor = 'text-blue-800';
+        this.paragraphColor = 'text-blue-800';
+        this.containerBackground = 'bg-blue-100';
+      }
+
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+      }
+      this.timeoutId = setTimeout(() => {
+        this.removeOldestNotification();
+      }, 2000);
+    },
+    showPositionNotification(title, text, type = null, position = 'top-left') {
+      const notification = {
+        id: this.notificationId++,
+        title,
+        text,
+        type,
+        position,
+        visible: true
+      };
+      this.notifications.unshift(notification);
+
+      if (type === 'dark') {
+        this.pageBackground = 'bg-gray-800';
+        this.headingColor = 'text-sky-400';
+        this.paragraphColor = 'text-sky-400';
+        this.containerBackground = 'bg-gray-700';
+      } else if (type === 'light') {
+        this.pageBackground = 'bg-blue-200';
+        this.headingColor = 'text-blue-800';
+        this.paragraphColor = 'text-blue-800';
+        this.containerBackground = 'bg-blue-100';
+      }
 
       if (this.timeoutId) {
         clearTimeout(this.timeoutId);
@@ -88,24 +152,13 @@ export default {
   border-radius: 5px;
   transition: opacity 1s;
 }
-.notification.success {
-  background-color: #d4edda;
-  border-color: #c3e6cb;
-  color: #155724;
-}
-.notification.danger {
-  background-color: #f8d7da;
-  border-color: #f5c6cb;
-  color: #721c24;
-}
-.notification.warning {
-  background-color: #fff3cd;
-  border-color: #ffeeba;
-  color: #856404;
-}
 .notification.dark {
-  background-color: #343a40;
-  border-color: #343a40;
-  color: #fff;
+  background-color: #1f2937;
+  border-color: #38BDF8;
+  color: #38BDF8;
+}
+.notification.light {
+  border-color: #1e3a8a;
+  color: #1e3a8a;
 }
 </style>
