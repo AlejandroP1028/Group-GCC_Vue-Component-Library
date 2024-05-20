d087ae9ff9057cbcb704dcd68d64eb698a32ad8d
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-21 02:55:51 +0800

    updated card components

--- src/alert_test.vue
+++ src/alert_test.vue
@@ -1,34 +1,35 @@
 <template>
-  <div :class="{ 'dark': isDarkMode }" class="overflow-hidden h-screen w-screen bg-gray-100 dark:bg-gray-900">
+  <div :class="{ 'dark': isDarkMode }" class="overflow h-screen w-screen bg-gray-100 dark:bg-gray-900">
     <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
       {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
     </button>
-    <Alert msg="ooga booga 123" dismissType="manual"></Alert>
-    <Alert msg="ooga booga 123" position="tr" dismissType="manual" type="cyan" ></Alert>
-    <Alert msg="ooga booga 123" position="lr" dismissType="manual" type="sky" ></Alert>
-    <Alert msg="ooga booga 123" position="ll" type="teal" ></Alert>
-    <Card header="Head" body="asdasdas" size="xs" ></Card>
+    <Card header="Head" body="asdasdas" size="xs"></Card>
     <Card header="Head" body="asdasdas" type="sky" size="sm"></Card>
     <Card header="Head" body="asdasdas" type="cyan" size="md"></Card>
-    <Card 
-      header="Card Title" 
-      body="This is a card body" 
-      image="assets/image1.jpg" 
-      size="lg" 
-      type="sky">
-      <button class="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Click Me</button>
+    <Card
+      header="Card Title"
+      body="This is a card body"
+      image="image-1.jpg"
+      size="sm"
+      type="teal">
+      <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
     </Card>
   </div>
 </template>
 
 <script>
-import Alert from './components/Alert.vue';
+// import Alert from './components/Alert.vue';
 import Card from './components/Card.vue';
 
 export default {
   name: 'AlertTest',
   components: {
-    Alert,
+    // Alert,
     Card
   },
   data() {
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,70 +1,76 @@
 <template>
-    <div :class="['m-4 p-6 border rounded-lg shadow', computedClassesMain]">
-      <a href="#">
-        <img v-if="image" :src="image" class="rounded-t-lg" alt="Card Image" />
-      </a>
-      <h5 v-if="header" class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">{{ header }}</h5>
-      <p v-if="body" class="mb-3 font-normal text-gray-700 dark:text-gray-200">{{ body }}</p>
-      <slot></slot>
+  <div :class="['border rounded-lg shadow', computedClassesMain]">
+    <div class="flex justify-center">
+      <img v-if="image" :src="imageSrc" class="rounded-t-lg" alt="Card Image" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'CardComponent',
-    props: {
-      header: String,
-      body: String,
-      image: String,
-      size: {
-        type: String,
-        validator: function (value) {
-          return ['xs', 'sm', 'md', 'lg', 'xl'].includes(value);
-        },
-        default: 'sm',
+
+    <div class="p-4">
+      <h5 v-if="header" class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">{{ header }}</h5>
+    <p v-if="body" class="mb-3 font-normal text-gray-700 dark:text-gray-200">{{ body }}</p>
+    <slot></slot>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    header: String,
+    body: String,
+    image: String,
+    size: {
+      type: String,
+      validator: function (value) {
+        return ['xs', 'sm', 'md', 'lg', 'xl'].includes(value);
       },
-      type: {
-        type: String,
-        validator: function (value) {
-          return ['default', 'sky', 'cyan', 'teal'].includes(value);
-        },
-        default: 'default',
-      },
+      default: 'sm',
     },
-    computed: {
-      computedClassesMain() {
-        const styleClasses = {
-          default: {
-            bgClass: 'border-blue-50 bg-blue-100 dark:bg-blue-800 dark:border-blue-700',
-          },
-          sky: {
-            bgClass: 'border-sky-50 bg-sky-100 dark:bg-sky-800 dark:border-sky-700',
-          },
-          cyan: {
-            bgClass: 'border-cyan-50 bg-cyan-100 dark:bg-cyan-800 dark:border-cyan-700',
-          },
-          teal: {
-            bgClass: 'border-teal-50 bg-teal-100 dark:bg-teal-800 dark:border-teal-700',
-          },
-        };
-  
-        const { bgClass } = styleClasses[this.type] || styleClasses['default'];
-  
-        return [
-          {
-            'max-w-xs': this.size === 'xs',
-            'max-w-sm': this.size === 'sm' || !['xs', 'sm', 'md', 'lg', 'xl'].includes(this.size),
-            'max-w-md': this.size === 'md',
-            'max-w-lg': this.size === 'lg',
-            'max-w-xl': this.size === 'xl',
-          },
-          bgClass,
-        ];
+    type: {
+      type: String,
+      validator: function (value) {
+        return ['default', 'sky', 'cyan', 'teal'].includes(value);
       },
+      default: 'default',
     },
-  };
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  },
+  computed: {
+    computedClassesMain() {
+      const styleClasses = {
+        default: {
+          bgClass: 'border-blue-100 bg-blue-200/80 dark:bg-blue-800/80 dark:border-blue-700',
+        },
+        sky: {
+          bgClass: 'border-sky-100 bg-sky-200/80 dark:bg-sky-800/80 dark:border-sky-700',
+        },
+        cyan: {
+          bgClass: 'border-cyan-100 bg-cyan-200/80 dark:bg-cyan-800/80 dark:border-cyan-700',
+        },
+        teal: {
+          bgClass: 'border-teal-100 bg-teal-200/80 dark:bg-teal-800/80 dark:border-teal-700',
+        },
+      };
+
+      const { bgClass } = styleClasses[this.type] || styleClasses['default'];
+
+      return [
+        {
+          'max-w-xs': this.size === 'xs',
+          'max-w-sm': this.size === 'sm' || !['xs', 'sm', 'md', 'lg', 'xl'].includes(this.size),
+          'max-w-md': this.size === 'md',
+          'max-w-lg': this.size === 'lg',
+          'max-w-xl': this.size === 'xl',
+        },
+        bgClass,
+      ];
+    },
+    imageSrc() {
+      return this.image ? require(`@/assets/${this.image}`) : '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
