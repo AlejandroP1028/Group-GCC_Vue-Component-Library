291d65e9d3cdc8fa4e0464f6f17fe9488c31be64
parruchoPLP <parrucho_jhermaine@plpasig.edu.ph>
2024-05-22 00:38:00 +0800

    version x.5

--- src/dropdown_test.vue
+++ src/dropdown_test.vue
@@ -1,12 +1,21 @@
 <template>
-  <div :class="{ 'dark': isDarkMode }" class="overflow-hidden h-screen w-screen bg-gray-100 dark:bg-gray-900">
-    <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+  <div :class="{ 'dark': isDarkMode }" class="overflow-hidden h-screen w-screen bg-gray-100 dark:bg-gray-900 flex flex-col items-center justify-start">
+    <button @click="toggleDarkMode" type="button" class="fixed bottom-4 right-4 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
       {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
     </button>
-    <dropdown_component :isDark="isDarkMode"></dropdown_component>
+    <div class="text-justify mb-4 max-w-4xl mt-20 relative">
+      <h1 :class="['text-4xl', 'mb-2', 'font-bold', headingColor]">Dropdown</h1>
+      <p :class="['mb-4', paragraphColor]">
+        The Dropdown Component is a versatile element designed for displaying a list of options or actions in a toggleable menu. It supports different types of notifications such as success, error, warning, and info messages, making it essential for user interaction.
+      </p>
+      <div class="absolute w-full">
+        <dropdown_component type="teal" size="l" :isDark="isDarkMode" bordered="true"></dropdown_component>
+      </div>
+      <h1 :class="['text-2xl', 'mb-2', 'mt-4', 'font-bold', 'text-center', headingColor]">Default Dropdown</h1>
+    </div>
   </div>
 </template>
-  
+
 <script>
 import dropdown_component from './components/Dropdown.vue';
 
@@ -17,7 +26,9 @@ export default {
   },
   data() {
     return {
-      isDarkMode: false // Initial state is light mode
+      isDarkMode: false,
+      headingColor: 'text-gray-900 dark:text-gray-100',
+      paragraphColor: 'text-gray-700 dark:text-gray-300'
     };
   },
   methods: {
@@ -36,4 +47,3 @@ export default {
   color: #ffffff;
 }
 </style>
-  
