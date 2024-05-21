0a4f818bb138af1a5f69fa3ca05f8137ba8bbcf7
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-05-21 21:42:44 +0800

    Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav :class="[`navbar`]">
+    <div class="flex flex-row">
+      <div></div>
+
+      <div>
+        <ul>
+          <li>hajdhasjdas</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/navbar_test.vue
@@ -0,0 +1,13 @@
+<template>
+  <navbar></navbar>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
