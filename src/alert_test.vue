935d0698c6138b7555bf51449228db94df1eb594
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-21 19:01:57 +0800

    changed font styling card

--- src/alert_test.vue
+++ src/alert_test.vue
@@ -5,30 +5,36 @@
     <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
       {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
     </button>
-    <Alert msg="ooga booga 123" position="tr" dismiss-type="manual">
+    <!-- <Alert msg="ooga booga 123" position="tr" dismiss-type="manual">
     </Alert>
     <Alert msg="ooga booga 123" position="ll" type="warning"  dismiss-type="manual">
     </Alert>
     <Alert msg="ooga booga 123" position="tl" type="danger"  dismiss-type="manual">
     </Alert>
     <Alert msg="ooga booga 123" position="lr" type="success"  dismiss-type="manual" size="l">
-    </Alert>
+    </Alert> -->
 
 
 
-    <!-- <div class="flex flex-row space-x-4">
+    <div class="flex flex-row space-x-4">
       <Card
         header="Card Title"
-        body="This is a card body"
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et sem malesuada, sollicitudin magna a, commodo leo. Suspendisse dapibus sodales risus, ut varius mi imperdiet." 
         image="image-1.jpg"
-        size="xs">
+        size="lg">
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Read more
+        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </a>
 
       </Card>
       <Card
         header="Card Title"
         body="This is a card body"
         image="image-1.jpg"
-        size="xs"
+        size="xl"
         type="sky">
 
       </Card>
@@ -36,7 +42,7 @@
         header="Card Title"
         body="This is a card body"
         image="image-1.jpg"
-        size="xs"
+        size="md"
         type="cyan">
 
       </Card>
@@ -44,23 +50,23 @@
         header="Card Title"
         body="This is a card body"
         image="image-1.jpg"
-        size="xs"
+        size="sm"
         type="teal">
 
       </Card>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-// import Card from './components/Card.vue';
-import Alert from './components/Alert.vue';
+import Card from './components/Card.vue';
+// import Alert from './components/Alert.vue';
 
 export default {
   name: 'App',
   components: {
-    Alert,
-    // Card
+    // Alert,
+    Card
   },
   data() {
     return {
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['border rounded-lg shadow max-h-fit', computedClassesMain]">
+  <div :class="['border rounded-lg shadow max-h-auto', computedClassesMain]">
     <div class="flex justify-center">
       <img v-if="image" :src="imageSrc" class="rounded-t-lg" alt="Card Image" />
     </div>
@@ -36,74 +36,42 @@ export default {
   computed: {
     computedClassesMain() {
       const styleClasses = {
-        default: {
-          bgClass: 'border-blue-200/[.80] bg-blue-100 dark:bg-gray-700 dark:border-blue-300',
-        },
-        sky: {
-          bgClass: 'border-sky-200/[.80] bg-sky-100 dark:bg-gray-700 dark:border-sky-300',
-        },
-        cyan: {
-          bgClass: 'border-cyan-200/[.80] bg-cyan-100 dark:bg-gray-700 dark:border-cyan-300',
-        },
-        teal: {
-          bgClass: 'border-teal-200/[.80] bg-teal-100 dark:bg-gray-700 dark:border-teal-300',
-        },
+        default: 'border-blue-200/[.80] bg-blue-100 dark:bg-gray-700 dark:border-blue-300',
+        sky: 'border-sky-200/[.80] bg-sky-100 dark:bg-gray-700 dark:border-sky-300',
+        cyan: 'border-cyan-200/[.80] bg-cyan-100 dark:bg-gray-700 dark:border-cyan-300',
+        teal: 'border-teal-200/[.80] bg-teal-100 dark:bg-gray-700 dark:border-teal-300',
       };
 
-      const { bgClass } = styleClasses[this.type] || styleClasses['default'];
+      const bgClass = styleClasses[this.type] || styleClasses['default'];
+      const sizeClasses = {
+        xs: 'max-w-xs',
+        sm: 'max-w-sm',
+        md: 'max-w-md',
+        lg: 'max-w-lg',
+        xl: 'max-w-xl',
+      };
 
-      return [
-        {
-          'max-w-xs': this.size === 'xs',
-          'max-w-sm': this.size === 'sm' || !['xs', 'sm', 'md', 'lg', 'xl'].includes(this.size),
-          'max-w-md': this.size === 'md',
-          'max-w-lg': this.size === 'lg',
-          'max-w-xl': this.size === 'xl',
-        },
-        bgClass,
-      ];
+      return [bgClass, sizeClasses[this.size]];
     },
     computedClassesHeader() {
       const styleClasses = {
-        default: {
-          textClass: 'text-blue-900/[.87] dark:text-blue-100/[.87]',
-        },
-        sky: {
-          textClass: 'text-sky-900/[.87] dark:text-sky-100/[.87]',
-        },
-        cyan: {
-          textClass: 'text-cyan-900/[.87] dark:text-cyan-100/[.87]',
-        },
-        teal: {
-          textClass: 'text-teal-900/[.87] dark:text-teal-100/[.87]',
-        },
+        default: 'text-blue-900/[.87] dark:text-blue-200',
+        sky: 'text-sky-900/[.87] dark:text-sky-200',
+        cyan: 'text-cyan-900/[.87] dark:text-cyan-200',
+        teal: 'text-teal-900/[.87] dark:text-teal-200',
       };
 
-      const { textClass } = styleClasses[this.type] || styleClasses['default'];
-      return [
-        textClass
-      ];
+      return styleClasses[this.type] || styleClasses['default'];
     },
     computedClassesBody() {
       const styleClasses = {
-        default: {
-          textClass: 'text-blue-900/[.60]  dark:text-blue-200/[.60]',
-        },
-        sky: {
-          textClass: 'text-sky-900/[.60] dark:text-sky-200/[.60]',
-        },
-        cyan: {
-          textClass: 'text-cyan-900/[.60] dark:text-cyan-200/[.60]',
-        },
-        teal: {
-          textClass: 'text-teal-900/[.60] dark:text-teal-200/[.60]',
-        },
+        default: 'text-blue-900/[.87] dark:text-blue-200/[.87]',
+        sky: 'text-sky-900/[.87] dark:text-sky-200/[.87]',
+        cyan: 'text-cyan-900/[.87] dark:text-cyan-200/[.87]',
+        teal: 'text-teal-900/[.87] dark:text-teal-200/[.87]',
       };
 
-      const { textClass } = styleClasses[this.type] || styleClasses['default'];
-      return [
-        textClass
-      ];
+      return styleClasses[this.type] || styleClasses['default'];
     },
     imageSrc() {
       return this.image ? require(`@/assets/${this.image}`) : '';
