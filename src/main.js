ecc59ee3b6c52600f5428e761281fedf745a52fd
Polbinsent <javier_paulvincent@plpasig.edu.ph>
2024-05-21 09:39:03 +0800

    Modified

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import CollapseTest from "./collapse_test.vue";
 //import ToastTest from "./toast_test.vue";
-// import CardTest from "./card_test.vue";
+
 import './index.css'
 
 createApp(CollapseTest).mount('#app')
 //CreateApp(ToastTest).mount*('#app')
-// createApp(CardTest).mount('#app')
