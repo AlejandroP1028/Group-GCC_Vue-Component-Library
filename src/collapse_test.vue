dfe4785f94a721c8b5026e949d376387ac054d9a
Polbinsent <javier_paulvincent@plpasig.edu.ph>
2024-05-21 09:32:23 +0800

    CollapseComponent

---
+++ src/collapse_test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="collapse_test">
+      <CollapseComponent>
+        <p>This is the content that will be collapsed and expanded.</p>
+        <p>You can place any content here.</p>
+      </CollapseComponent>
+      
+    </div>
+  </template>
+  
+  <script>
+  import CollapseComponent from './components/CollapseComponent.vue';
+  
+  export default {
+    name: 'collapse_test',
+    components: {
+      CollapseComponent
+    }
+  };
+  </script>
