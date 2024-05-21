23edf8efa99dedfe1db928f87738463df603ac8f
jiyancarloo <166023368+jiyancarloo@users.noreply.github.com>
2024-05-21 22:16:12 +0800

    Added component avatar

---
+++ src/avatar_test.vue
@@ -0,0 +1,50 @@
+<script setup>
+import Avatar from './components/UserAvatar.vue';
+
+</script>
+
+<template>
+  <div class="container mx-auto flex flex-col items-center justify-center h-screen">
+    <div class="flex justify-between items-end space-x-1">
+      <Avatar
+        name="Gian Catalan"
+        image="https://i.pinimg.com/originals/04/63/12/046312aac440d95cbc6d56b3eb527d5d.jpg"
+        size="sm"
+        rounded="full"
+        :is-online="true"/>
+        
+        <Avatar
+        name="Gian Catalan"
+        image="https://i.pinimg.com/originals/04/63/12/046312aac440d95cbc6d56b3eb527d5d.jpg"
+        size="md"
+        rounded="full"
+        :is-online="true"/>
+
+        <Avatar
+        name="Gian Catalan"
+        image="https://i.pinimg.com/originals/04/63/12/046312aac440d95cbc6d56b3eb527d5d.jpg"
+        size="lg"
+        rounded="full"
+        :is-online="true"/>
+
+    
+
+    </div>
+  
+    <div class="flex justify-between items-end space-x-1 mt-4">
+      <Avatar
+        name="Alejandro Prado"
+        size="sm"
+        rounded="full"
+        :is-online="true"/>
+      <Avatar
+        name="Gian Catalan"
+        rounded="full"
+        size="md"/>
+      <Avatar
+        name="Ryan Intalan"
+        size="lg"/>
+    </div>
+  </div>
+</template>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
-import AlertTest from "./alert_test.vue";
+//import AlertTest from "./alert_test.vue";
 // import CardTest from "./card_test.vue";
 import './index.css'
 
-createApp(AlertTest).mount('#app')
+import AvatarTest from "./avatar_test.vue";
+
+//createApp(AlertTest).mount('#app')
 // createApp(CardTest).mount('#app')
+
+createApp(AvatarTest).mount('#app')
