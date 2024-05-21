d6e3909d96ae93a6eaa5074790d2498564077d3f
France Alcantara <164699947+ffranceal@users.noreply.github.com>
2024-05-21 13:57:38 +0800

    modified:   src/components/AccordionComponent.vue

--- src/components/AccordionComponent.vue
+++ src/components/AccordionComponent.vue
@@ -6,7 +6,7 @@
     <v-expansion-panels>
       <v-expansion-panel
         title="Title"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima">
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptati/;.lp.lp.bus! Eaque cupiditate minima">
       </v-expansion-panel>
     </v-expansion-panels>
   </div>
