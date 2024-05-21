a2619cc420f057e84482565fbc0b41294256f640
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-05-22 00:15:05 +0800

    Navbar component updates

--- src/navbar_test.vue
+++ src/navbar_test.vue
@@ -1,5 +1,19 @@
 <template>
-  <navbar></navbar>
+  <section
+    class="page flex flex-col items-center justify-center h-screen overflow-hidden"
+  >
+    <h1 class="text-2xl">Navbar</h1>
+    <div>
+      <h1>Default Navbar</h1>
+      <div class="demo p-10 bg-gray-100 w-auto">
+        <div class="w-auto">
+          <header class="bg-white p-4">
+            <navbar></navbar>
+          </header>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
