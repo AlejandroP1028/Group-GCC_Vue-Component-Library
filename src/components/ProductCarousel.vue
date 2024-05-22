8862ec952dc904183828a0558ed2950da376c7df
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-22 16:10:26 +0800

    added hover effect

--- src/components/ProductCarousel.vue
+++ src/components/ProductCarousel.vue
@@ -3,7 +3,7 @@
     <!-- First Carousel -->
     <Carousel :value="products" :numVisible="1" :numScroll="1" orientation="vertical" verticalViewPortHeight="330px" contentClass="flex items-center justify-start" style="margin-top: 50px; margin-left: -59%;">
         <template #item="slotProps">
-          <div class="rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #dbeafe, #C3DDFD);">
+          <div class="first-carousel rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #dbeafe, #C3DDFD);">
             <div class="mb-3">
               <div class="relative mx-auto max-w-xs max-h-50">
                 <img :src="slotProps.data.image" :alt="slotProps.data.name" class="w-60 h-48 object-contain " />
@@ -14,8 +14,8 @@
             <div class="flex justify-between items-center">
               <div class="mt-0 font-bold text-blue-900">${{ slotProps.data.price }}</div>
               <span>
-                <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-400 border-white" />
-                <Button icon="pi pi-shopping-cart" class="ml-2 text-blue-400 border-white"/>
+                <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-500 border-white" />
+                <Button icon="pi pi-shopping-cart" class="ml-2 text-blue-900 border-white"/>
               </span>
             </div>
           </div>
@@ -25,19 +25,19 @@
     <!-- Second Carousel -->
       <Carousel :value="products" :numVisible="1" :numScroll="1" orientation="vertical" verticalViewPortHeight="330px" contentClass="flex items-center" style="margin-top: -27.8%; margin-left: -19%;">
         <template #item="slotProps">
-          <div class="rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #e0f2fe, #75A5F5);">
+          <div class="second-carousel rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #e0f2fe, #75A5F5);">
             <div class="mb-3">
               <div class="relative mx-auto max-w-xs max-h-50">
                 <img :src="slotProps.data.image" :alt="slotProps.data.name" class="w-60 h-48 object-contain rounded-md" />
                 <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:0px; top: 5px"/>
               </div>
             </div>
-            <div class="mb-4 font-bold text-blue-800 text-xs">{{ slotProps.data.name }}</div>
+            <div class="mb-4 font-bold text-sky-900 text-xs">{{ slotProps.data.name }}</div>
             <div class="flex justify-between items-center">
-              <div class="mt-0 font-bold text-blue-700">${{ slotProps.data.price }}</div>
+              <div class="mt-0 font-bold text-sky-900">${{ slotProps.data.price }}</div>
               <span>
-                <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-500 border-black" />
-                <Button icon="pi pi-shopping-cart" class="ml-2 text-blue-500 border-white"/>
+                <Button icon="pi pi-heart" severity="secondary" outlined class="text-sky-700" />
+                <Button icon="pi pi-shopping-cart" class="ml-2 text-sky-900 border-white"/>
               </span>
             </div>
           </div>
@@ -48,19 +48,19 @@
       <!-- Third Carousel -->
       <Carousel :value="products" :numVisible="1" :numScroll="1" orientation="vertical" verticalViewPortHeight="330px" contentClass="flex items-center" style="margin-top: -27.8%; margin-left: 22%;">
         <template #item="slotProps">
-          <div class="rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #ecfeff, #99f6e4);">
+          <div class="third-carousel rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #ecfeff, #99f6e4);">
             <div class="mb-3">
               <div class="relative mx-auto max-w-xs max-h-50">
                 <img :src="slotProps.data.image" :alt="slotProps.data.name" class="w-60 h-48 object-contain rounded-md" />
                 <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:0px; top: 5px"/>
               </div>
             </div>
-            <div class="mb-4 font-bold text-cyan-600 text-xs">{{ slotProps.data.name }}</div>
+            <div class="mb-4 font-bold text-cyan-900 text-xs">{{ slotProps.data.name }}</div>
             <div class="flex justify-between items-center">
-              <div class="mt-0 font-bold text-cyan-700">${{ slotProps.data.price }}</div>
+              <div class="mt-0 font-bold text-cyan-900">${{ slotProps.data.price }}</div>
               <span>
-                <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-400 border-black" />
-                <Button icon="pi pi-shopping-cart" class="ml-2 text-blue-500 border-white"/>
+                <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-500 border-black" />
+                <Button icon="pi pi-shopping-cart" class="ml-2 text-cyan-900 border-white"/>
               </span>
             </div>
           </div>
@@ -71,7 +71,7 @@
      <!-- Fourth Carousel -->
       <Carousel :value="products" :numVisible="1" :numScroll="1" orientation="vertical" verticalViewPortHeight="330px" contentClass="flex items-center" style="margin-top: -27.8%; margin-left: 63%;">
         <template #item="slotProps">
-          <div class="rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #f0fdfa, #a5f3fc);">
+          <div class="fourth-carousel rounded-md m-2 p-3 font-poppins shadow-xl" style="background-image: linear-gradient(to right, #f0fdfa, #a5f3fc);">
             <div class="mb-3">
               <div class="relative mx-auto max-w-xs max-h-50">
                 <img :src="slotProps.data.image" :alt="slotProps.data.name" class="w-60 h-48 object-contain rounded-md" />
@@ -80,10 +80,10 @@
             </div>
             <div class="mb-4 font-bold text-teal-900 text-xs">{{ slotProps.data.name }}</div>
             <div class="flex justify-between items-center">
-              <div class="mt-0 font-bold text-teal-700">${{ slotProps.data.price }}</div>
+              <div class="mt-0 font-bold text-teal-900">${{ slotProps.data.price }}</div>
               <span>
                 <Button icon="pi pi-heart" severity="secondary" outlined class="text-blue-500 border-black" />
-                <Button icon="pi pi-shopping-cart" class="ml-2 text-blue-500 border-white"/>
+                <Button icon="pi pi-shopping-cart" class="ml-2 text-teal-900 border-white"/>
               </span>
             </div>
           </div>
@@ -121,28 +121,18 @@ export default {
 </script>
 
 <style scoped>
-.carousel-wrapper {
-  width: 20%;
-  border-radius: 20px;
-  margin-top: 100px;
-  margin-left: 100px;
-  background-image: linear-gradient(to right, #dbeafe, #C3DDFD);
-  box-shadow: 0 15px 21px rgba(0, 0, 0, 0.3);
+
+.first-carousel,
+.second-carousel,
+.third-carousel,
+.fourth-carousel {
   transition: transform 0.3s;
 }
 
-.carousel-wrapper-2 {
-  position: absolute;
-  width: 20%;
-  border-radius: 20px;
-  margin-top: -28%;
-  margin-left: 470px;
-  background-image: linear-gradient(to right, #e0f2fe, #75A5F5);
-  box-shadow: 0 15px 21px rgba(0, 0, 0, 0.3);
-  transition: transform 0.3s;
-}
-
-.carousel-wrapper:hover {
+.first-carousel:hover,
+.second-carousel:hover,
+.third-carousel:hover,
+.fourth-carousel:hover {
   transform: scale(1.1);
 }
 
@@ -153,4 +143,6 @@ export default {
 .font-poppins {
   font-family: 'Poppins', sans-serif;
 }
+
+
 </style>
