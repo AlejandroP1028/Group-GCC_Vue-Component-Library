f1bf0dbcf2f60e7c8eb584689951ac1706337861
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-22 14:37:36 +0800

    modified:   src/AppNotifications.vue 	modified:   src/components/NotificationsGroup.vue

--- src/AppNotifications.vue
+++ src/AppNotifications.vue
@@ -1,8 +1,14 @@
 <template>
+  <div :class="{ 'dark': isDarkMode }" class="overflow-hidden h-screen w-screen bg-gray-100 dark:bg-gray-900">
+    <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 bg-gray-800 hover:bg-blue-800 text-blue-200 font-bold py-2 px-4 rounded">
+      {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+    </button>
   <Notifications type="success" notification_message ="This is a success notification." position="top-right"></Notifications>
-  <Notifications type="error" notification_message ="This is an error notification." position="bottom-right"></Notifications>
-  <Notifications type="warning" notification_message ="This is a warning notification" position="bottom-left"></Notifications>
-  <Notifications type="default" notification_message ="This is a default notification" position="center"></Notifications>
+  <Notifications type="error" notification_message ="This is an error notification." position="top-right1"></Notifications>
+  <Notifications type="warning" notification_message ="This is a warning notification" position="top-right2"></Notifications>
+  <Notifications type="default" notification_message ="Default notification or messages" position="center"></Notifications>
+  <Notifications type="info" notification_message ="Default notification or messages" position="bottom-right"></Notifications>
+  </div>
 </template>
 
 <script>
@@ -12,6 +18,24 @@ export default {
 name: 'AppNotifications',
 components: {
   Notifications
-}
+},
+data() {
+    return {
+      isDarkMode: false 
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+    }
+  }
 }
 </script>
+
+<style scoped>
+/* Dark mode styles */
+.dark {
+  background-color: #091224;
+  color: #38BDF8;
+}
+</style>
--- src/components/NotificationsGroup.vue
+++ src/components/NotificationsGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :id="`toast-${type}`"
-    class="fixed flex items-center w-full max-w-xs p-4 mb-4 rounded-lg shadow"
+    class="fixed flex items-center w-full max-w-xs p-4 mb-4 rounded-full shadow"
     :class="[positionClass, notificationBgClass, notificationTextColorClass]"
     role="alert"
   >
@@ -9,12 +9,50 @@
       class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg"
       :class="iconBgClass"
     >
+      <svg
+        v-if="type === 'success'"
+        class="w-5 h-5 text-blue-200 rounded-lg dark:text-sky-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+        />
+      </svg>
+      <svg
+        v-if="type === 'error'"
+        class="w-5 h-5 text-blue-900 rounded-lg dark:text-sky-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"
+        />
+      </svg>
+      <svg
+        v-if="type === 'warning'"
+        class="w-5 h-5 text-blue-700 rounded-lg dark:text-sky-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"
+        />
+      </svg>
     </div>
-    <div class="ms-3 text-sm font-normal">{{ notification_message }}</div>
+    <div class="ms-3 text-sm font-normal">
+      {{ notification_message }}
+    </div>
     <button
+      v-if="type !== 'default'"
       type="button"
-      class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 focus:ring-blue-500 p-1.5 inline-flex items-center justify-center h-8 w-8 hover:bg-blue-500"
-
+      class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 focus:ring-blue-500 p-1.5 inline-flex items-center justify-center h-8 w-8 hover:bg-blue-400"
       :data-dismiss-target="`#toast-${type}`"
       aria-label="Close"
     >
@@ -45,7 +83,7 @@ export default {
     type: {
       type: String,
       required: true,
-      validator: (value) => ["success", "error", "warning", "default"].includes(value),
+      validator: (value) => ["success", "error", "warning", "default", "delete"].includes(value),
     },
     notification_message: {
       type: String,
@@ -54,24 +92,16 @@ export default {
     position: {
       type: String,
       default: "bottom-right",
-      validator: (value) => ["top-left", "top-right", "bottom-left", "bottom-right", "center"].includes(value),
+      validator: (value) => ["top-left", "top-right1", "top-right2", "top-right", "bottom-right", "center"].includes(value),
     },
   },
   computed: {
-    iconBgClass() {
-      const bgClasses = {
-        success: "bg-blue-800",
-        error: "bg-blue-200",
-        warning: "bg-blue-700",
-        default: "bg-blue-900",
-      };
-      return bgClasses[this.type];
-    },
     positionClass() {
       const positions = {
-        "top-left": "top-4 left-4",
         "top-right": "top-4 right-4",
-        "bottom-left": "bottom-4 left-4",
+        "top-right1":"top-20 right-4 mt-4",
+        "top-right2":"top-24 right-4 mt-20",
+        "top-left": "top-4 left-4",
         "bottom-right": "bottom-4 right-4",
         "center": "top-4 left-1/2 transform -translate-x-1/2",
       };
@@ -79,21 +109,30 @@ export default {
     },
     notificationBgClass() {
       const bgClasses = {
-        success: "bg-blue-200",
-        error: "bg-blue-400",
-        warning: "bg-blue-300",
-        default: "bg-blue-100",
+        success: "bg-blue-100 dark:bg-gray-800",
+        error: "bg-blue-200 dark:bg-gray-800",
+        warning: "bg-blue-300 dark:bg-gray-800",
+        default: "bg-blue-800 dark:bg-gray-800",
       };
       return bgClasses[this.type];
     },
     notificationTextColorClass() {
       const textClasses = {
-        success: "text-blue-800",
-        error: "text-blue-200",
-        warning: "text-blue-700",
-        default: "text-blue-900",
+        success: "text-blue-800 dark:text-sky-400",
+        error: "text-blue-900  dark:text-sky-400",
+        warning: "text-blue-800  dark:text-sky-400",
+        default: "text-blue-100  dark:text-sky-400",
       };
       return textClasses[this.type];
+    },
+    iconBgClass() {
+      const bgClasses = {
+        success: "bg-blue-800 rounded-2xl dark:bg-gray-700",
+        error: "bg-blue-100 rounded-2xl dark:bg-gray-700",
+        warning: "bg-blue-200 rounded-2xl dark:bg-gray-700",
+        default: "",
+      };
+      return bgClasses[this.type];
     },
   },
 };
