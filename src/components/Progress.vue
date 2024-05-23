a4b84f2e4c05c51f789ccb2963853ae9000fc372
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-23 22:28:02 +0800

    Create Progress.vue

---
+++ src/components/Progress.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+      <div
+        :class="progressClasses"
+        :style="{ width: computedWidth }"
+      ></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Progress',
+    props: {
+      progress: {
+        type: Number,
+        required: true,
+        validator: value => value >= 0 && value <= 100,
+      },
+      color: {
+        type: String,
+        default: 'blue',
+        validator: value => ['blue', 'sky', 'cyan', 'teal', 'gray'].includes(value),
+      },
+    },
+    computed: {
+      computedWidth() {
+        return `${this.progress}%`;
+      },
+      progressClasses() {
+        const colorClasses = {
+          blue: 'bg-blue-600',
+          sky: 'bg-sky-600',
+          cyan: 'bg-cyan-600',
+          teal: 'bg-teal-600',
+          gray: 'bg-gray-600',
+        };
+        return [colorClasses[this.color], 'h-2.5', 'rounded-full'].join(' ');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
