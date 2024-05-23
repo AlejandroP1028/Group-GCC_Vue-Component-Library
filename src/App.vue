85fa1151f6eaedd40a5c85b2c4b2c5abdcb5538e
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-23 22:28:42 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <template>
-  <div id="app">
-    <ProductCarousel :products="products" />
-    <ProgressBars/>
+  <div :class="{ 'dark': isDarkMode }" class="overflow-auto h-screen w-screen bg-blue-50 dark:bg-gray-900 p-4">
+    <div class="flex justify-center mb-8">
+      <Button
+        @click="toggleDarkMode"
+        color="blue"
+        customClasses="w-48"
+      >
+        {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+      </Button>
+    </div>
+    <div class="space-y-4">
+      <Progress :progress="45" color="blue" />
+      <Progress :progress="60" color="sky" />
+      <Progress :progress="75" color="cyan" />
+      <Progress :progress="30" color="teal" />
+      <Progress :progress="90" color="gray" />
+    </div>
   </div>
 </template>
 
 <script>
-import ProductCarousel from './components/ProductCarousel.vue';
-import ProgressBars from './components/ProgressBars.vue';
+import Progress from './components/Progress.vue';
 
 export default {
   name: 'App',
   components: {
-    ProductCarousel,  
-    ProgressBars
+    // Button,
+    Progress,
   },
   data() {
     return {
-      products: [
-        { name: 'Blueberry Cheesecake', image: 'https://www.bluezones.com/wp-content/uploads/2016/10/Mini-Blueberry-Cheesecakes-e1511160203205.jpg', inventoryStatus: 'AVAILABLE', price: 100 },
-        { name: 'Chocolate Mousse', image: 'https://www.bluezones.com/wp-content/uploads/2018/03/chocolate-mousse.jpg', inventoryStatus: 'LOWSTOCK', price: 50 },
-        { name: 'Black Bean Brownies', image: 'https://www.bluezones.com/wp-content/uploads/2016/10/The-Balanced-Blonde_Chocolate-Chunk-Black-Bean-Brownies.jpg', inventoryStatus: 'OUTOFSTOCK', price: 70 },
-      ]
+      isDarkMode: false,
     };
-  }
+  },
+  mounted() {
+    this.checkDarkModePreference();
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      this.updateDarkModePreference();
+    },
+    checkDarkModePreference() {
+      const darkModePreference = localStorage.getItem('darkMode');
+      if (darkModePreference) {
+        this.isDarkMode = JSON.parse(darkModePreference);
+      }
+    },
+    updateDarkModePreference() {
+      localStorage.setItem('darkMode', JSON.stringify(this.isDarkMode));
+    },
+  },
 };
 </script>
 
-<style>
-@import './assets/tailwind.css';
+<style scoped>
+.dark {
+  background-color: #121212;
+  color: #ffffff;
+}
 </style>
