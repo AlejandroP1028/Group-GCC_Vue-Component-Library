814629dadf4e89aa411d06c7e5c2c91dc9f16137
Polbinsent <javier_paulvincent@plpasig.edu.ph>
2024-05-24 01:50:17 +0800

    First Modification

--- src/collapse_test.vue
+++ src/collapse_test.vue
@@ -1,20 +1,92 @@
 <template>
+  <div :class="{ 'dark': isDarkMode }" class="overflow-auto h-screen w-screen bg-gray-100 dark:bg-gray-900">
+    <button @click="toggleDarkMode" type="button" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+      {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+    </button>
     <div id="collapse_test">
-      <CollapseComponent>
-        <p>This is the content that will be collapsed and expanded.</p>
-        <p>You can place any content here.</p>
+      <CollapseComponent :type="'default'" :isDarkMode="isDarkMode" id="collapse1">
+        <p>
+          This is type 1 container content
+          This is type 1 container content
+          This is type 1 container content
+        </p>
       </CollapseComponent>
-      
+      <CollapseComponent :type="'type1'" :isDarkMode="isDarkMode" id="collapse2">
+        <p>
+          This is type 2 container content
+          This is type 2 container content
+          This is type 2 container content
+        </p>
+      </CollapseComponent>
+      <CollapseComponent :type="'type2'" :isDarkMode="isDarkMode" id="collapse3">
+        <p>
+          This is type 3 container content
+          This is type 3 container content
+          This is type 3 container content
+        </p>
+      </CollapseComponent>
+      <CollapseComponent :type="'type3'" :isDarkMode="isDarkMode" id="collapse4">
+        <p>
+          This is type 4 container content
+          This is type 4 container content
+          This is type 4 container content
+        </p>
+      </CollapseComponent>
     </div>
-  </template>
-  
-  <script>
-  import CollapseComponent from './components/CollapseComponent.vue';
-  
-  export default {
-    name: 'collapse_test',
-    components: {
-      CollapseComponent
+    <AlertComponent message="This is an alert message!" />
+  </div>
+</template>
+
+<script>
+import CollapseComponent from './components/CollapseComponent.vue';
+import AlertComponent from './components/Alert.vue';
+
+export default {
+  name: 'CollapseTest',
+  components: {
+    CollapseComponent,
+    AlertComponent
+  },
+  data() {
+    return {
+      isDarkMode: false
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
     }
-  };
-  </script>
+  }
+};
+</script>
+
+<style scoped>
+.dark {
+  background-color: #121212;
+  color: #ffffff;
+}
+
+#collapse1 {
+  position: fixed;
+  top: 25%;
+  left: 25%;
+}
+
+#collapse2 {
+  position: fixed;
+  top: 25%;
+  left: 75%;
+}
+
+#collapse3 {
+  position: fixed;
+  top: 75%;
+  left: 25%;
+}
+
+#collapse4 {
+  position: fixed;
+  top: 75%;
+  left: 75%;
+}
+</style>
--- src/components/CollapseComponent.vue
+++ src/components/CollapseComponent.vue
@@ -1,6 +1,6 @@
 <!--Collapse HTML Structure-->
 <template>
-  <div class="collapse-container">
+  <div :class="['collapse-container', containerTypeClass]">
     <button @click="toggle" class="collapse-button">
       {{ isCollapsed ? show : hide }}
     </button>
@@ -26,13 +26,26 @@
       hide: {
         type: String,
         default: 'Hide'
-      }
+      },
+      type: {
+      type: String,
+      default: 'default'
+      },
+      isDarkMode: {
+      type: Boolean,
+      default: false
+    }
     },
     data() {
       return {
         isCollapsed: this.initialCollapsed
       };
     },
+    computed: {
+    containerTypeClass() {
+      return `collapse-container-${this.type}`;
+    }
+  },
     methods: {
       toggle() {
         this.isCollapsed = !this.isCollapsed;
@@ -42,11 +55,7 @@
 </script>
 
 <style scoped>
-.collapse-container {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-}
+/* COLLAPSE STYLE */
 
 .collapse-button {
   display: inline-block;
@@ -63,20 +72,53 @@
 
 .collapse-content {
   max-height: fit-content;
-  max-width: fit-content;
-  font-family: Georgia, serif;
-  color: #2c3e50;
-  font-size:medium;
+  max-width: 300px;
   margin-top: 10px;
   overflow: hidden;
-  background-color: #edf2fb;
   padding: 5px 8px;
-  border: 4px double #03045e;
-  border-radius: 3px;
   margin-top: 5px;
   transition: max-height 0.2s ease-out;
 }
+/* COLLAPSE CONTAINER STYLE */
+.collapse-container-default .collapse-content {
+  background-color:  #C3DDFD;
+  border: 2.5px solid #2563eb;
+}
 
+.collapse-container-type1 .collapse-content {
+  background-color: #bae6fd;
+  border: 2.5px solid #0284c7;
+  border-radius: 20px;
+}
+
+.collapse-container-type2 .collapse-content {
+  background-color: #a5f3fc;
+  border: 4px solid #0891b2;
+  border-radius: 10px;
+}
+.collapse-container-type3 .collapse-content {
+  background-color: #99f6e4;
+  border: 4px solid #0d9488;
+  border-radius: 30px;
+}
+
+/* Dark mode  */
+.dark .collapse-container-default .collapse-content  {
+  background-color: #2563eb;
+  border: 4px solid #C3DDFD;
+}
+.dark .collapse-container-type1 .collapse-content  {
+  background-color: #0284c7;
+  border: 4px solid #bae6fd;
+}
+.dark .collapse-container-type2 .collapse-content  {
+  background-color: #0891b2;
+  border: 4px solid #a5f3fc;
+}
+.dark .collapse-container-type3 .collapse-content  {
+  background-color: #0d9488;
+  border: 4px solid #99f6e4;
+}
 .collapse-enter-active, .collapse-leave-active {
   transition: ease;
 }
@@ -84,5 +126,6 @@
 .collapse-enter, .collapse-leave-to {
   max-height: 0;
 }
+
 </style>
   
