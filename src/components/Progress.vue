8563db4119b356f1b89bd512b50f084b0016c91f
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-24 00:20:46 +0800

    Update Progress.vue

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+    <div class="relative w-80 bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
+        <span v-if="labelInside" class="absolute top-0 left-5 -mt-0.6 text-white px-1" style="font-size: 10px">{{ labelInside }}</span>
+
       <div
         :class="progressClasses"
         :style="{ width: computedWidth }"
@@ -19,7 +21,15 @@
       color: {
         type: String,
         default: 'blue',
-        validator: value => ['blue', 'sky', 'cyan', 'teal', 'gray'].includes(value),
+        validator: value => ['blue', 'sky', 'cyan', 'teal', 'gray', 'gradientblue'].includes(value),
+      },
+      labelInside: {
+        type: String,
+        default: '',
+      },
+      gradient: {
+        type: Boolean,
+        default: false,
       },
     },
     computed: {
@@ -27,19 +37,31 @@
         return `${this.progress}%`;
       },
       progressClasses() {
+        const baseClass = ['h-3.5', 'rounded-full'];
+        if (this.gradient) {
+          return [
+           ...baseClass,
+            'bg-gradient-to-r',
+            'from-blue-500',
+            'via-sky-500',
+            'to-cyan-200',
+          ];
+        }
         const colorClasses = {
           blue: 'bg-blue-600',
           sky: 'bg-sky-600',
           cyan: 'bg-cyan-600',
           teal: 'bg-teal-600',
           gray: 'bg-gray-600',
+          gradientblue: 'bg-blue-600',
         };
-        return [colorClasses[this.color], 'h-2.5', 'rounded-full'].join(' ');
+        return [colorClasses[this.color],...baseClass].join(' ');
       },
     },
   };
   </script>
   
   <style scoped>
+
   </style>
   
