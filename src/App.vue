e4d688d9cc62d6f8d71ee6049544efd110143ed4
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-24 00:21:19 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,37 @@
         color="blue"
         customClasses="w-48"
       >
-        {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+        {{ isDarkMode? 'Light Mode' : 'Dark Mode' }}
       </Button>
     </div>
     <div class="space-y-4">
+      <b>Different Colors </b>  <br>
       <Progress :progress="45" color="blue" />
       <Progress :progress="60" color="sky" />
       <Progress :progress="75" color="cyan" />
       <Progress :progress="30" color="teal" />
-      <Progress :progress="90" color="gray" />
+      <Progress :progress="90" color="gray" /> 
+
+      <br>
+
+      <b>Styled Bar with Label Inside</b>
+
+      <Progress :progress="70" color="orange" gradient="true" labelInside="70%" />
+      <Progress :progress="85" color="orange" gradient="true"  labelInside="85% " />
+      <Progress :progress="25" color="orange" gradient="true"  labelInside="25% " />
+      <Progress :progress="45" color="orange" gradient="true"  labelInside="45% " />
+      <Progress :progress="65" color="orange" gradient="true"  labelInside="65% " />
     </div>
   </div>
 </template>
 
 <script>
 import Progress from './components/Progress.vue';
+// Ensure Button is imported or defined here if needed
 
 export default {
   name: 'App',
   components: {
-    // Button,
     Progress,
   },
   data() {
@@ -38,7 +49,7 @@ export default {
   },
   methods: {
     toggleDarkMode() {
-      this.isDarkMode = !this.isDarkMode;
+      this.isDarkMode =!this.isDarkMode;
       this.updateDarkModePreference();
     },
     checkDarkModePreference() {
@@ -59,4 +70,5 @@ export default {
   background-color: #121212;
   color: #ffffff;
 }
+
 </style>
