b44141469a42c62e912766ba6b763d1c35d21d7d
Lovelyannn <164727963+Lovelyannn@users.noreply.github.com>
2024-05-25 00:08:38 +0800

    modified both components

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,19 @@
       </Button>
     </div>
     <ButtonGroup class="mx-auto">
-      <button type="button" :class="profileButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-800 bg-blue-200 border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+      <button type="button" color="blue" :class="profileButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium border border-gray-200 rounded-s-lg hover:bg-blue-600 focus:ring-blue-300 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
         <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
         </svg>
         Profile
       </button>
-      <button type="button" :class="settingsButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-800 bg-blue-200 border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+      <button type="button" color="blue" :class="settingsButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium border-t border-b border-gray-200 hover:bg-blue-600 focus:ring-blue-300 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
         <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
         </svg>
         Settings
       </button>
-      <button type="button" :class="downloadsButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-800 bg-blue-200 border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+      <button type="button" color="blue" :class="downloadsButtonClasses" class="inline-flex items-center px-4 py-2 text-sm font-medium border border-gray-200 rounded-e-lg hover:bg-blue-600 focus:ring-blue-300 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
         <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
           <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
@@ -170,7 +170,6 @@ export default {
   color: #ffffff;
 }
 .mx-auto {
-  margin-top: -3%;
   margin-bottom: 3%;
   margin-right: -38%;
 }
