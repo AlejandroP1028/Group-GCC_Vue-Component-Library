411a73be98191bc35117e61b17b60bb40667b28e
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-26 21:12:41 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import Carousel from 'primevue/carousel';
-import Button from 'primevue/button';
-import Tag from 'primevue/tag';
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import './assets/style.css';
+// import testCarousel from "./testCarousel.vue";
+import testProgress from "./testProgress.vue";
+import './assets/tailwind.css';
 
-import BootstrapVue3 from 'bootstrap-vue-3'
 
-const app = createApp(App);
-app.use(PrimeVue);
+// createApp(testCarousel).mount('#app')
+createApp(testProgress).mount('#app')
 
-app.component('Carousel', Carousel);
-app.component('Button', Button);
-app.component('Tag', Tag);
-app.use(BootstrapVue3)
 
-app.mount('#app');
