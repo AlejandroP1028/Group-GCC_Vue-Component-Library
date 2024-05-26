02b544ae62d93adbc6e62c11d981fa4f6eb9a1e1
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-26 20:21:06 +0800

    Merge branch 'main' of https://github.com/AlejandroP1028/Group-GCC_Vue-Component-Library into arnau_components

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="flex flex-col items-center h-full">
-    <div v-if="type === 'unorderedlist'" class="rounded-lg shadow p-4 bg-blue-200 dark:bg-gray-800 mb-8">
+    <div v-if="type === 'unorderedlist'" class="rounded-lg shadow p-4 bg-blue-200 dark:bg-gray-800 mb-8" :style="{ width: containerWidth }">
       <h2 class="mb-2 text-lg font-semibold text-blue-900 dark:text-sky-400">Unordered List:</h2>
-      <ul class="max-w-md space-y-1 text-blue-800 list-disc list-inside dark:text-sky-400">
+      <ul class="space-y-1 text-blue-800 list-disc list-inside dark:text-sky-400">
         <li v-for="(item, index) in items" :key="index">
           {{ item }}
         </li>
       </ul>
     </div>
 
-    <div v-else-if="type === 'orderedlist'" class="rounded-lg shadow p-4 bg-blue-200 dark:bg-gray-800 mb-8">
+    <div v-else-if="type === 'orderedlist'" class="rounded-lg shadow p-4 bg-blue-200 dark:bg-gray-800 mb-8" :style="{ width: containerWidth }">
       <h2 class="mb-2 text-lg font-semibold text-blue-900 dark:text-sky-400">Ordered List:</h2>
-      <ol class="max-w-md space-y-1 text-blue-800 list-decimal list-inside dark:text-sky-400">
+      <ol class="space-y-1 text-blue-800 list-decimal list-inside dark:text-sky-400">
         <li v-for="(item, index) in items" :key="index">
           {{ item }}
         </li>
       </ol>
     </div>
 
-    <div v-else-if="type === 'descriptionlist'" class="rounded-lg shadow-lg p-4 bg-blue-200 dark:bg-gray-800 mb-8">
-      <div class="max-w-md text-blue-900 divide-y divide-blue-300 dark:text-sky-400 dark:divide-gray-700">
+    <div v-else-if="type === 'descriptionlist'" class="rounded-lg shadow-lg p-4 bg-blue-200 dark:bg-gray-800 mb-8" :style="{ width: containerWidth }">
+      <div class="text-blue-900 divide-y divide-blue-300 dark:text-sky-400 dark:divide-gray-700">
         <div class="flex flex-col pb-3" v-for="(item, index) in items" :key="index">
           <dt class="mb-1 text-blue-900 font-semibold md:text-lg dark:text-sky-300">{{ item.term }}</dt>
           <dd class="text-lg font-bold">{{ item.description }}</dd>
@@ -42,6 +42,11 @@ export default {
       required: true,
       validator: value => ["unorderedlist", "orderedlist", "descriptionlist"].includes(value),
     },
+  },
+  data() {
+    return {
+      containerWidth: '400px'
+    };
   },
 };
 </script>
