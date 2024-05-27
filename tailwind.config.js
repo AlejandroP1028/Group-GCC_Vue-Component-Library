f68d1accdbbdb2567815a61e21dc4fd4ec68dea1
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-28 00:50:19 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
+  darkMode: 'selector',
   theme: {
     extend: {
       animation: {
@@ -19,4 +20,3 @@ module.exports = {
   },
   plugins: [],
 };
-
