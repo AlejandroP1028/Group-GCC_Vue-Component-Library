f8ad718043a5b6f66be3378f4123bcf3c88ae9ca
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-27 18:46:10 +0800

    added template_page vue for reference

--- src/template_page.vue
+++ src/template_page.vue
@@ -1,5 +1,9 @@
 <template>
     <!--
+        main div <div :class="{ 'dark': isDarkMode }" class="overflow-auto h-screen w-screen bg-blue-50 dark:bg-gray-900 flex flex-row scroll-smooth">
+        </div>
+    -->
+    <!--
         If you are going to use external icons use this cdn
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         this will allow you to use font-awesome icons 
@@ -11,6 +15,22 @@
         header="GCC Alert" 
         body="Show contextual information to your users using alert elements based on Tailwind CSS"/>
     -->
+
+    <!--
+        Sections can be used 2 ways first is the normal section:
+
+        <Section header="Alert Dismiss Type" 
+        @sectioncreated="addLink" <------- this is very important!!!
+        body="The GCC alert component supports manual and automatic dismissals. Use the following button to show random alert components with their respective dismissal type.">
+        <template #content> enter your content here </template>
+
+        and just the body text
+        <Section body="The GCC alert component can be used to provide information to your users such as success or error messages, 
+        but also highlighted information complementing the normal flow of paragraphs and headers on a page."/>
+        
+    -->
+
+
 
 
     <!--
@@ -46,16 +66,20 @@ export default{
     name: 'component-name_page',
     data(){
         return{
-            links: []
+            links: [],
+            isDarkMode: false,
         }
     },
     methods: {
         addLink(header) {
       this.links.push({ label: header });
     },
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+    }
     },
 }
 </script>
 
-<style>
+<style scoped>
 </style>
