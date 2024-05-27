002db41926c162debce3d931420e60cb794a1afa
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-27 19:47:13 +0800

    new file:   src/List_test.vue 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 //para lang makita ko sa browser changes nung 2 components
 import { createApp } from 'vue'
-import App from "./App.vue";
+//import App from "./App.vue";
 import './index.css'
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
 
-//import AppNotifications from './AppNotifications.vue'; 
-//createApp(AppNotifications).mount('#app');
+//import ListTest from "./List_test.vue";
+//createApp(ListTest).mount('#app')
+
+import AppNotifications from './AppNotifications.vue'; 
+createApp(AppNotifications).mount('#app');
 
 // createApp(AlertTest).mount('#app')
 //createApp(CardTest).mount('#app')
