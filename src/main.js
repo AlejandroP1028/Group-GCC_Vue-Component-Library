4156207a835f4ae5b0f30594e5ffbeec30d550b8
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-29 00:42:31 +0800

    added test.vue to test positions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
+ /* eslint-disable */
+
 import { createApp } from 'vue'
 //import App from "./App.vue";
 import './index.css'
+import test from './test.vue'
 //createApp(App).mount('#app')
 
 //import ListTest from "./List_test.vue";
 //createApp(ListTest).mount('#app')
 import AppNotifications from './AppNotifications.vue'; 
-createApp(AppNotifications).mount('#app');
+createApp(test).mount('#app');
 
 // createApp(AlertTest).mount('#app')
 //createApp(CardTest).mount('#app')
