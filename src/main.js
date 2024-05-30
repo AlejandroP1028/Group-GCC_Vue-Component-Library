45954aab124123c0aae1716914c5148529780407
Alejandro Pogi <knggrim@gmail.com>
2024-05-30 17:21:12 +0800

    added template with content

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@
 
 import { createApp } from 'vue'
 import AlertTest from "./alert_page.vue";
+import card from "./card_page.vue"
 import app from "./App.vue"
 // import CardTest from "./card_test.vue";
 import './index.css'
 
 // import AvatarTest from "./avatar_test.vue";
 
-createApp(AlertTest).mount('#app')
+createApp(card).mount('#app')
 // createApp(CardTest).mount('#app')
 
 // createApp(AvatarTest).mount('#app')
