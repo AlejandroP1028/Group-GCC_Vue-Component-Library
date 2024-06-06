8c1aa24feee7bdb11e1de6452cb293a219fac756
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-06-06 23:19:27 +0800

    modified:   src/notif_page.vue

--- src/notif_page.vue
+++ src/notif_page.vue
@@ -11,8 +11,8 @@
               <Button color="blue" :class="'w-9/10'" @click="showNotification">Show Default Notification</Button>
             </div>
             <div :class="{ 'dark': defaultDark }" class="relative bg-gray-200 dark:bg-gray-800 transition-all duration-300 ease-in-out mt-4 w-full h-96 p-4 overflow-hidden rounded-lg shadow-lg border border-blue-600/[.87]">
-              <Notifications v-if="notificationVisible" type="default" notification_message="This is a default notification." position="centered"/> 
               <Notifications type="default" notification_message="This is a default notification." position="centered"/>
+              <Notifications v-for="(notification, index) in notifications" :key="index" :type="notification.type" :notification_message="notification.message" position="centered"/>
             </div>
           </div>
         </template>
@@ -28,11 +28,7 @@
               <Button color="cyan" :class="'w-9/10'" @click="showNotificationCyan">Cyan</Button>
             </div>
             <div :class="{ 'dark': defaultDark }" class="relative bg-gray-200 dark:bg-gray-800 transition-all duration-300 ease-in-out mt-4 w-full h-96 p-4 overflow-hidden rounded-lg shadow-lg border border-blue-600/[.87]">
-              <Notifications v-if="notificationDefaultVisible" type="default" notification_message="This is a default notification." position="centered"/> 
-              <Notifications v-if="notificationBlueBorderVisible"  type="BlueBorder" notification_message="This is a blue bordered default notification." position="centered"/>       
-              <Notifications v-if="notificationCyanBorderVisible" type="CyanBorder" notification_message="This is a cyan bordered default notification." position="centered"/>
-              <Notifications v-if="notificationSkyBorderVisible" type="SkyBorder" notification_message="This is a sky bordered default notification." position="centered"/>     
-              <Notifications v-if="notificationTealBorderVisible" type="TealBorder" notification_message="This is a teal bordered default notification." position="centered"/>    
+              <Notifications v-for="(notification, index) in notificationsDefaultStyle" :key="index" :type="notification.type" :notification_message="notification.message" position="centered"/>
             </div>
           </div>
         </template>
@@ -44,8 +40,7 @@
               <Button color="blue" :class="'w-9/10'" @click="showNotificationDismiss">Show Notification Dismiss</Button>
             </div>
             <div :class="{ 'dark': defaultDark }" class="relative bg-gray-200 dark:bg-gray-800 transition-all duration-300 ease-in-out mt-4 w-full h-96 p-4 overflow-hidden rounded-lg shadow-lg border border-blue-600/[.87]">
-              <Notifications type="dismiss" notification_message="This is a notification with dismissible close button." position="centered"/> 
-              <Notifications v-if="notificationDismiss"  type="dismiss" notification_message="This is a notification with dismissible close button." position="centered"/>       
+              <Notifications v-for="(notification, index) in notificationsDismiss" :key="index" :type="notification.type" :notification_message="notification.message" position="centered"/>
             </div>
           </div>
         </template>
@@ -53,11 +48,16 @@
       <Section header="Notification Types" @sectioncreated="addLink" body="The GCC Notification Component encompasses various notification types with dismissible close button tailored to different scenarios, ensuring users are promptly informed and can respond effectively to system events. The success notification provides reassurance by signaling successful operations, the error notification promptly alerts users to encountered errors, the warning notification preemptively highlights potential issues or risks, the message notification that informs a user that a message is sent/received.">
         <template #content>
           <div class="flex flex-col items-center mt-4 h-full">
-            <div :class="{ 'dark': defaultDark }" class="relative bg-gray-200 dark:bg-gray-800 transition-all duration-300 ease-in-out mt-4 w-full h-96 p-4 overflow-hidden rounded-lg shadow-lg border border-blue-600/[.87]">
-              <Notifications type="success" notification_message="This is a success notification." position="pTop-right"/>       
-              <Notifications type="warning" notification_message="This is a warning notification." position="pTop-left"/>
-              <Notifications type="error" notification_message="This is an error notification." position="pBottom-left"/>     
-              <Notifications type="message" notification_message="This is a message notification." position="pBottom-right"/>    
+            <div class="flex space-x-4">
+              <Button color="blue" :class="'w-9/10'" @click="showMessage">Message</Button>
+              <Button color="sky" :class="'w-9/10'" @click="showWarning">Warning</Button>
+              <Button color="teal" :class="'w-9/10'" @click="showError">Error</Button>
+              <Button color="cyan" :class="'w-9/10'" @click="showSuccess">Success</Button>
+              <Button color="blue" :class="'w-9/10'" @click="showAll">Show All Notifications</Button>
+
+            </div>
+            <div :class="{ 'dark': defaultDark }" class="relative bg-gray-200 dark:bg-gray-800 transition-all duration-300 ease-in-out mt-4 w-full h-96 p-4 overflow-hidden rounded-lg shadow-lg border border-blue-600/[.87]">  
+              <Notifications v-for="(notification, index) in notificationsTypes" :key="index" :type="notification.type" :notification_message="notification.message" :position="notification.position"/>    
             </div>
           </div>
         </template>
@@ -90,7 +90,7 @@
       </div>
       <hr class="h-0.5 bg-blue-600 border-none rounded-full">
     </div>
-  </div>  
+  </div>
 </template>
 
 <script>
@@ -114,14 +114,10 @@ export default {
       links: [],
       isDarkMode: false,
       defaultDark: false,
-      notificationVisible: false,
-      notificationVisibled: false,
-      notificationDefaultVisible: false,
-      notificationBlueBorderVisible: false,
-      notificationCyanBorderVisible: false,
-      notificationSkyBorderVisible: false,
-      notificationTealBorderVisible: false,
-      notificationDismiss: false,
+      notifications: [],
+      notificationsDefaultStyle: [],
+      notificationsDismiss: [],
+      notificationsTypes: [],
       notificationPositionsA: [],
       notificationPositionsP: []
     };
@@ -134,61 +130,62 @@ export default {
       this.links.push({ label: header });
     },
     showNotification() {
-      this.notificationVisible = !this.notificationVisible;
+      this.notifications.push({ type: 'default', message: 'This is a default notification.' });
     },
     showNotificationDefault() {
-      this.notificationDefaultVisible = !this.notificationDefaultVisible;
-      this.notificationBlueVisible = false;
-      this.notificationCyanBorderVisible = false;
-      this.notificationSkyBorderVisible = false;
-      this.notificationTealBorderVisible = false;
+      this.notificationsDefaultStyle.push({ type: 'default', message: 'This is a default notification.' });
     },
     showNotificationBlue() {
-      this.notificationBlueBorderVisible = !this.notificationBlueBorderVisible;
-      this.notificationDefaultVisible = false;
-      this.notificationCyanBorderVisible = false;
-      this.notificationSkyBorderVisible = false;
-      this.notificationTealBorderVisible = false;
+      this.notificationsDefaultStyle.push({ type: 'BlueBorder', message: 'This is a blue bordered default notification.' });
     },
     showNotificationSky() {
-      this.notificationSkyBorderVisible = !this.notificationSkyBorderVisible;
-      this.notificationDefaultVisible = false;
-      this.notificationCyanBorderVisible = false;
-      this.notificationBlueBorderVisible = false;
-      this.notificationTealBorderVisible = false;
+      this.notificationsDefaultStyle.push({ type: 'SkyBorder', message: 'This is a sky bordered default notification.' });
     },
     showNotificationCyan() {
-      this.notificationCyanBorderVisible = !this.notificationCyanBorderVisible;
-      this.notificationDefaultVisible = false;
-      this.notificationBlueBorderVisible = false;
-      this.notificationSkyBorderVisible = false;
-      this.notificationTealBorderVisible = false;
+      this.notificationsDefaultStyle.push({ type: 'CyanBorder', message: 'This is a cyan bordered default notification.' });
     },
     showNotificationTeal() {
-      this.notificationTealBorderVisible = !this.notificationTealBorderVisible;
-      this.notificationDefaultVisible = false;
-      this.notificationCyanBorderVisible = false;
-      this.notificationSkyBorderVisible = false;
-      this.notificationBlueBorderVisible = false;
+      this.notificationsDefaultStyle.push({ type: 'TealBorder', message: 'This is a teal bordered default notification.' });
     },
     showNotificationDismiss() {
-      this.notificationDismiss = !this.notificationDismiss;
+      this.notificationsDismiss.push({ type: 'dismiss', message: 'This is a notification with a dismissible close button.' });
     },
+    showMessage() {
+      this.notificationsTypes.push({ type: 'message', message: 'This is a message notification.', position: 'pBottom-right' });
+    },
+    showSuccess() {
+      this.notificationsTypes.push({ type: 'success', message: 'This is a success notification.', position: 'pTop-right' });
+    },
+    showError() {
+      this.notificationsTypes.push({ type: 'error', message: 'This is an error notification.', position: 'pBottom-left' });
+    },
+    showWarning() {
+      this.notificationsTypes.push({ type: 'warning', message: 'This is a warning notification.', position: 'pTop-left' });
+    },
+    showAll() {
+    this.notificationsTypes.push(
+      { type: 'message', message: 'This is a message notification.', position: 'pBottom-right' },
+      { type: 'success', message: 'This is a success notification.', position: 'pTop-right' },
+      { type: 'error', message: 'This is an error notification.', position: 'pBottom-left' },
+      { type: 'warning', message: 'This is a warning notification.', position: 'pTop-left' }
+    );
+  },
     showPositions(type) {
       const notificationTypes = [
         { type: 'message', message: 'This is a message notification.', position: type === 'absolute' ? 'bottom-right' : 'pBottom-right' },
         { type: 'warning', message: 'This is a warning notification.', position: type === 'absolute' ? 'top-left' : 'pTop-left' },
         { type: 'error', message: 'This is an error notification.', position: type === 'absolute' ? 'bottom-left' : 'pBottom-left' },
         { type: 'success', message: 'This is a success notification.', position: type === 'absolute' ? 'top-right' : 'pTop-right' }
-    ]; 
-    notificationTypes.forEach(notificationType => {
-      if (type === 'absolute') {
-        this.notificationPositionsA.push({ ...notificationType });
-      } else {
-        this.notificationPositionsP.push({ ...notificationType });
-      }
-    });
+      ]; 
+      notificationTypes.forEach(notificationType => {
+        if (type === 'absolute') {
+          this.notificationPositionsA.push({ ...notificationType });
+        } else {
+          this.notificationPositionsP.push({ ...notificationType });
+        }
+      });
     }
   }
 };
 </script>
+
