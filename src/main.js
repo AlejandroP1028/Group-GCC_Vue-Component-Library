d60d47a2c061becf28602b5b1d1f208e930d1c04
jiyancarloo <166023368+jiyancarloo@users.noreply.github.com>
2024-06-07 22:45:13 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
 import { createApp } from 'vue'
-import AlertTest from "./alert_page.vue";
-import app from "./App.vue"
+//import AlertTest from "./alert_page.vue";
+//import app from "./App.vue"
 // import CardTest from "./card_test.vue";
 import './index.css'
 
-// import AvatarTest from "./avatar_test.vue";
+ import AvatarTest from "./avatar_test.vue";
 
-createApp(AlertTest).mount('#app')
+//createApp(AlertTest).mount('#app')
 // createApp(CardTest).mount('#app')
 
-// createApp(AvatarTest).mount('#app')
+ createApp(AvatarTest).mount('#app')
