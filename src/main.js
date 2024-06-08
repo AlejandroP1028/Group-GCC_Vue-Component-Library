fbb5ebc39e66e9b71fcbf589e4d7a90d432d3e5f
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-06-09 01:26:16 +0800

    navbar_page

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 /* eslint-disable */
 
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import alert_page from "./alert_page.vue";
-import badge_page from './badge_page.vue';
-import breadcrumb_page from './breadcrumb_page.vue';
-import card_page from './card_page.vue';
-import './index.css'
-import dropdown_test from './dropdown_test.vue';
+import badge_page from "./badge_page.vue";
+import breadcrumb_page from "./breadcrumb_page.vue";
+import card_page from "./card_page.vue";
+import "./index.css";
+import dropdown_test from "./dropdown_test.vue";
+import navbar_page from "./navbar_page.vue";
 
-createApp(alert_page).mount('#app')
-// createApp(breadcrumb_page).mount('#app')
+//createApp(alert_page).mount("#app");
+createApp(navbar_page).mount("#app");
+//createApp(breadcrumb_page).mount("#app");
 //createApp(dropdown_test).mount('#app')
