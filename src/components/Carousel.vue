b28bf69d4287daab3c995c8ebc73aab5f050a2de
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-06-09 23:59:37 +0800

    Update Carousel.vue

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,10 @@
-<!-- Carousel.vue -->
 <template>
-  <div :class="['relative w-full', borderSize, borderColor, { 'dark:border-gray-800': isDarkMode }]" data-carousel="static">
-    <!-- Add new buttons for controlling slider -->
-    <button v-if="showSliderControls" @click="toggleSliderControls" class="absolute top-0 end-0 z-30 bg-white dark:bg-gray-800 p-2 m-2 rounded-md shadow-md cursor-pointer focus:outline-none">
-      {{ showSlider ? 'Remove Slider Control' : 'Revert Slider Control' }}
+  <div :class="['relative w-full', borderSize, borderColor, {'dark:border-gray-800': isDarkMode}]" data-carousel="static">
+    <button v-if="showSliderControls" @click="toggleSliderControls" class="absolute top-0 end-0 z-30 bg-white dark:bg-gray-800 dark:text-white p-2 m-2 rounded-md shadow-md cursor-pointer focus:outline-none">
+      {{ showSlider? 'Remove Slider Control' : 'Revert Slider Control' }}
     </button>
-    
-    <!-- Existing carousel content -->
-    <!-- Carousel wrapper -->
     <div class="relative h-full overflow-hidden">
       <transition-group name="carousel" tag="div">
-        <!-- Carousel items -->
         <div
           v-for="(item, index) in items"
           :key="index"
@@ -23,12 +17,14 @@
             class="w-full h-auto"
             style="max-height: 100%;"
           />
+          <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center">
+            <h3 class="text-lg font-semibold text-white dark:text-gray-800">{{ item.heading }}</h3>
+            <p class="text-sm text-white dark:text-gray-500">{{ item.body }}</p>
+          </div>
         </div>
       </transition-group>
     </div>
-    <!-- Slider indicators -->
     <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-      <!-- Slider indicator buttons -->
       <button
         v-for="(item, index) in items"
         :key="index"
@@ -40,7 +36,6 @@
         data-carousel-slide-to="index"
       ></button>
     </div>
-    <!-- Slider controls -->
     <button
       v-if="showSliderControls && showSlider"
       type="button"
@@ -49,7 +44,6 @@
       data-carousel-prev
     >
       <span :class="['inline-flex items-center justify-center w-10 h-10 rounded-full', sliderControlColor, 'group-hover:bg-cyan-400 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none']">
-        <!-- Previous button icon -->
         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
         </svg>
@@ -64,7 +58,6 @@
       data-carousel-next
     >
       <span :class="['inline-flex items-center justify-center w-10 h-10 rounded-full', sliderControlColor, 'group-hover:bg-cyan-400 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none']">
-        <!-- Next button icon -->
         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
         </svg>
@@ -92,11 +85,11 @@ export default {
       default: 'border-blue-600',
     },
     borderSize: {
-      type: String, 
+      type: String,
     },
     indicatorColor: {
       type: String,
-      default: 'bg-blue-500', 
+      default: 'bg-blue-500',
     },
     sliderControlColor: {
       type: String,
@@ -109,7 +102,7 @@ export default {
     autoplayInterval: {
       type: Number,
       default: 3000, // 3 seconds
-    }
+    },
   },
   data() {
     return {
@@ -122,7 +115,7 @@ export default {
   computed: {
     itemClasses() {
       return index => ({
-        hidden: this.currentSlide !== index,
+        hidden: this.currentSlide!== index,
         'duration-700 ease-in-out': true,
         block: this.currentSlide === index,
         'carousel-enter-active': true,
@@ -133,7 +126,7 @@ export default {
       return index => ({
         'w-3 h-3 rounded-full': true,
         [this.indicatorColor]: this.currentSlide === index,
-        'bg-gray-300': this.currentSlide !== index,
+        'bg-gray-300': this.currentSlide!== index,
       });
     },
   },
@@ -159,8 +152,7 @@ export default {
       this.autoplayTimer = null;
     },
     toggleSliderControls() {
-      this.showSlider = !this.showSlider;
-      this.showSliderControls = !this.showSliderControls;
+      this.showSlider =!this.showSlider;
     },
   },
   mounted() {
@@ -168,7 +160,7 @@ export default {
   },
   beforeUnmount() {
     this.stopAutoplay();
-  }
+  },
 };
 </script>
 
